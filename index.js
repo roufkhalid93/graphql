0ec45f0e44e46aebd4f395deb80b7dd15913af23
roufkhalid93 <roufkhalid93@gmail.com>
2025-02-17 16:55:17 +0800

    resolver functions

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ import {ApolloServer} from '@apollo/server'
 //allow to start the server
 import {startStandaloneServer} from '@apollo/server/standalone'
 
+//db
+import db from './_db.js'
+
 //types
-import { typeDefs } from './schema'
+import { typeDefs } from './schema.js'
 
-
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors(){
+            return db.authors
+        },
+        reviews(){
+            return db.reviews
+        }
+    }
+}
 
 //server setup
 const server = new ApolloServer({
-    //typeDefs
     typeDefs,
-    //resolvers
+    resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
     listen: {port: 4000}
 })
 
-console.log('Server ready at port', 4000)
+console.log(`Server ready at port', ${url}`)
