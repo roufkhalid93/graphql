ddf91110c8a6efca2fe0f13d0f34597e968659ef
roufkhalid93 <roufkhalid93@gmail.com>
2025-02-17 16:11:02 +0800

    schema & types

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ import {ApolloServer} from '@apollo/server'
 //allow to start the server
 import {startStandaloneServer} from '@apollo/server/standalone'
 
+//types
+import { typeDefs } from './schema'
+
+
+
 //server setup
 const server = new ApolloServer({
     //typeDefs
+    typeDefs,
     //resolvers
 })
 
-const { url } = await structuredClone(server, {
+const { url } = await startStandaloneServer(server, {
     listen: {port: 4000}
 })
 
